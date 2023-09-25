f972e28e26cb1950444ab12b07112b2548ef11aa
stand4rd <thomas.noble.jordan@gmail.com>
2023-09-25 14:42:17 -0700

    removed unnecessary files

--- mail.h
+++
@@ -1,9 +0,0 @@
-/* Header file for mail function */
-
-#ifndef mail_h
-#define mail_h
-
-// Function to send an email using Gmail API
-int sendEmail(const char *access_token, const char *email_message);
-
-#endif
